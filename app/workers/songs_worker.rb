aae260876816fef634ceda1c16826207ac570f4b
Branden Mosley <branden.mosley1337@gmail.com>
2018-09-27 18:00:05 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -2,7 +2,9 @@ class SongsWorker
   include Sidekiq::Worker
   
   def perform(song_list)
-     
+    CSV.foreach(song_list, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
+    end 
   end
   
 end
